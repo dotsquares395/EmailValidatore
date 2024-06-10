55161968c01abbfab65850d1615c15be45944d70
dotsquares395 <rohan.sharma@dotsquares.com>
2024-06-10 11:59:38 +0530

    Initial commit

---
+++ ValidatoreEmail/ValidatoreEmail.h
@@ -0,0 +1,18 @@
+//
+//  ValidatoreEmail.h
+//  ValidatoreEmail
+//
+//  Created by Hemraj Yogi on 10/06/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ValidatoreEmail.
+FOUNDATION_EXPORT double ValidatoreEmailVersionNumber;
+
+//! Project version string for ValidatoreEmail.
+FOUNDATION_EXPORT const unsigned char ValidatoreEmailVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ValidatoreEmail/PublicHeader.h>
+
+
